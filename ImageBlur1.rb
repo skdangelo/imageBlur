5386d6f93896a825016dd112136e084032e7aecb
Sandra D'Angelo <skdangelo@gmail.com>
2019-06-24 04:03:34 +0000

    Add image blur 1

---
+++ ImageBlur1.rb
@@ -0,0 +1,22 @@
+class Image
+    attr_accessor :image
+
+    def initialize(array)
+        self.image = array
+    end
+
+    def output_image
+      @image.each do |row|
+        puts row.join
+      end
+    end
+
+end
+
+image = Image.new([
+  [0,0,0,0],
+  [0,1,0,0],
+  [0,0,0,1],
+  [0,0,0,0]
+  ])
+image.output_image
