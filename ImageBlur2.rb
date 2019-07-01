f88bcd524a5d296409cd5c24bbb1f86f43885cc6
Sandra D'Angelo <skdangelo@gmail.com>
2019-07-01 07:53:05 +0000

    Added Image Blur Assignment 2

---
+++ ImageBlur2.rb
@@ -0,0 +1,47 @@
+class Image
+    attr_accessor :image
+
+    def initialize(array)
+        self.image = array
+    end
+
+    def output_image
+      @image.each do |row|
+        puts row.join
+      end
+    end
+
+    def find_trues
+      #find all the trues in the 2D array
+      true_arr = []
+      @image.each_with_index do |row, row_i|
+        row.each_with_index do |cell, col_i|
+          if cell == 1
+            true_arr << [row_i,col_i]
+          end
+        end
+      end
+      true_arr
+    end
+
+    def blur_image
+      true_arr = find_trues
+      true_arr.each do |row, col|          
+        @image[row-1][col] = 1 if row > 0
+        @image[row+1][col] = 1 if row < @image.length-1
+        @image[row][col+1] = 1 if col < @image[row].length-1
+        @image[row][col-1] = 1 if col > 0
+      end
+    end
+end
+
+
+image = Image.new([
+  [0,0,0,0],
+  [0,1,0,0],
+  [0,0,0,1],
+  [0,0,0,0]
+  ])
+
+image.blur_image
+image.output_image
